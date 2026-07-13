8de32834eb37d8988aabbd03dace1ec1803c2828
tintin4303 <nyunt.tin4303@gmail.com>
2026-07-13 14:03:47 +0700

    new pages

---
+++ src/app/contracts/page.tsx
@@ -0,0 +1,7 @@
+export default function ContractsPage() {
+    return (
+        <div>
+            <h1>Contracts</h1>
+        </div>
+    );
+}
---
+++ src/app/logs/page.tsx
@@ -0,0 +1,7 @@
+export default function LogsPage() {
+    return (
+        <div>
+            <h1>Logs</h1>
+        </div>
+    );
+}
